e2ed87c32562eecdcbb4beb265db17f5165cb2ba
Tamires Marques Bernardes <89873829+TamiresMarques@users.noreply.github.com>
2022-11-13 22:45:23 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+
+    double salarioBase = 0.0;
+    double salario = 0.0;
+    double gratificacao = 0.0;
+    double imposto  = 0.0;
+    
+    System.out.println("Digite o seu salário base:");
+    salarioBase = entrada.nextDouble();
+    
+    gratificacao = salarioBase + 50;
+    imposto = salarioBase * 0.1;
+    salario = gratificacao - imposto;
+   
+    System.out.println("O salário é de R$ " + salario + ".");
+
+    
+
+    //if (salárioFuncionário <= 500) { 
+      //novoSalário = porcentagem2 * salárioFuncionário + salárioFuncionário;
+   // } else {
+     // novoSalário = porcentagem1 * salárioFuncionário + salárioFuncionário;
+//    } 
+  
+  }
+}
